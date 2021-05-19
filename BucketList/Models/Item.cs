1651f55240539ad67cfcd0cb50935b667e5e4234
Joan54 <joan.vanhouten@gmail.com>
2021-05-19 13:12:54 +0200

    Add a Completed CheckBox to the NewItemPage

--- BucketList/Models/Item.cs
+++ BucketList/Models/Item.cs
@@ -4,8 +4,9 @@ namespace BucketList.Models
 {
     public class Item
     {
-        public string Id { get; set; }
-        public string Text { get; set; }
+        public string Id          { get; set; }
+        public string Text        { get; set; }
         public string Description { get; set; }
+        public bool   IsCompleted { get; set; }
     }
 }
